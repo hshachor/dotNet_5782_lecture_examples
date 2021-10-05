bec68576663144e73cac28ce954005888b82858c
Haim Shachor <hshachor@g.jct.ac.il>
2021-10-05 17:18:21 +0300

    Added introduction example (hello world and strings)

---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+System.Console.WriteLine("Hello world");
+/*
+string s;
+s = "abcd";
+/*
+s = "a+b\n=\tc";
+
+s = @"Here I can draw whatever I want:
+      *
+     * *
+    *   *
+   *     *
+  *       *
+ *         *
+*************";
+
+s = "In order to write double quote use \\\" in your writing";
+string s1 = s;
+s = @"In order to write double quote in verbatim string, use """" in your writing";
+string s2 = s;
+s = s1 + s2;
+
+System.Console.WriteLine(s);
+*/
+/*
+if (false){
+    Console.Write("Enter a: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Enter b: ");
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine($"The result is: {a + b}");
+}
+if (false){
+    int a = 2, b = 3, c = 'a';
+    System.Console.WriteLine($@"
+The expression ""a+b"" result, c in hexadecimal:
+{a + b}, 0x{c:X4}
+");
+
+}
+*/
