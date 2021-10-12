81b0cb025b0546494fabd37a99aeffaf7ec1813d
Haim Shachor <hshachor@g.jct.ac.il>
2021-10-12 22:52:54 +0300

    Prepare BasicCs example for class

---
+++ BasicCS/BasicCS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BasicCS
+{
+    class A
+    {
+        public int b;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A(); // or new() - find automatically the type.
+            A a1 = a;
+            Console.WriteLine(a.b);
+            a1.b = 1;
+            Console.WriteLine(a.b);
+
+            /*
+            a.
+            A.
+             */
+        }
+    }
+}
