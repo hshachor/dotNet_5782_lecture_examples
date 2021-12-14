07c3dc171237877e7e027df3622739a6fabf8577
Haim Shachor <hshachor@g.jct.ac.il>
2021-12-14 23:54:13 +0200

    Added Data-Binding examples

---
+++ DataBinding/GradStatResource/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace GradStatResource
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ DataBinding/StyleTarget/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace solid55
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var set = new Setter(Button.BackgroundProperty, Brushes.Yellow);
+            var style = new Style(typeof(Button), this.Resources["bottunStyle"] as Style);
+            style.Setters.Add(new Setter(Button.BackgroundProperty, Brushes.Yellow));
+            this.Resources["bottunStyle"] = style;
+//            var b = sender as Button;
+//            b.Background = Brushes.Yellow;
+        }
+    }
+}
